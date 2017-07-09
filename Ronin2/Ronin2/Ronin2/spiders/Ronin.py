7be8951906209ba37e06114ee49c56302f505234
AshMohanty <mohantay@gmail.com>
2017-07-09 13:12:21 -0700

    Add files via upload
    Finished Elwood and deletes all duplicate entries now

---
+++ Ronin2/Ronin2/Ronin2/spiders/Ronin.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Spider
+from scrapy.http import Request
+
+class RoninSpider(scrapy.Spider):
+    name = 'Ronin'
+    allowed_domains = ['ronindivision.com']
+    start_urls = ['http://ronindivision.com/collections/frontpage/']
+
+    def parse(self, response):
+        clothes = response.xpath('//*[@class="image"]/a/@href').extract()
+        for clothes in clothes:
+            absolute_url = response.urljoin(clothes)
+            yield Request(absolute_url, callback=self.parse_clothes)
+
+        #proccess next page
+        next_page_url = response.xpath('//*[@class="item link"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
+
+    def parse_clothes(self, response):
+        title = response.css('h1::text').extract_first()
+        price = response.xpath('//*[@class="price"]/text()').extract_first()
+        image_url = response.xpath('//*[@class="image featured"]/a/@href').extract_first()
+        image_url = image_url.replace('//', 'http://')
+        description = response.xpath('//*[@class="description"]/p/text()').extract()
+        destination_url = response.xpath('//link[@rel="canonical"]/@href').extract()
+        yield {
+                'description': description,
+                'destination_url': destination_url,
+                'id': 0,
+                'image_url': image_url,
+                'price': price,
+                'ranking': 0,
+                'title': title
+                }
